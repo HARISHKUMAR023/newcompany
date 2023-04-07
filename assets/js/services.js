1df663d48076ba2bdee61e98274b862e123eca22
Harishkumar <harishkumarsp023@gmail.com>
2023-04-07 08:32:13 +0530

    over all  structure is updated

---
+++ assets/js/services.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#button1").click(function() {
+        $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the <br> electrical connection between different electrical components like integrated circuits and discrete components</p>");
+
+    });
+    $("#button2").click(function() {
+      $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the electrical connection between different electrical</p>");
+    });
+    $("#button3").click(function() {
+      $("#text-display").html("<p>Ptechminds a premier provider of PCB design services.<br> We are well prepared to undertake your printed circuit board design challenges ranging from simple to complex designs</p><p>Rigid and flexible circuit technologies <br>Custom circuit design igh-speed signal design <br> Differential impedance controlFine pitch BGA layoutSensitive <br> analog circuitsHigh current/voltage designsDesign for manufacturability</p>");
+    });
+    $("#button4").click(function() {
+      $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the electrical connection between different electrical</p>");
+    });
+    $("#button5").click(function() {
+      $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the electrical connection between different electrical</p>");
+    });
+  });
