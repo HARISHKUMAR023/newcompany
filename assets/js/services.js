be529efe9548236d288468059b8883df4f5d4282
Harishkumar <harishkumarsp023@gmail.com>
2023-04-07 13:36:33 +0530

    some changes in stucture dute client feed back

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,18 +1,48 @@
 $(document).ready(function() {
     $("#button1").click(function() {
+      $("#text-title").html("<p></p>");
+      $("#image-display").attr("src", "./assets/img/Images/03_Engineering Service/services-banner.jpg");
         $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the <br> electrical connection between different electrical components like integrated circuits and discrete components</p>");
 
     });
     $("#button2").click(function() {
+      $("#text-title").html("<p></p>");
+      $("#image-display").attr("src", "./assets/img/Images/03_Engineering Service/services-banner.jpg");
       $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the electrical connection between different electrical</p>");
     });
     $("#button3").click(function() {
+      $("#text-title").html("<p> TECHMINDS is a premier provider of PCB design services.<br>We are well prepared to undertake your printed circuit board design challenges ranging from simple to complex designs</p>");
+      $("#image-display").attr("src", "./assets/img/Images/03_Engineering Service/services-banner.jpg");
       $("#text-display").html("<p>Ptechminds a premier provider of PCB design services.<br> We are well prepared to undertake your printed circuit board design challenges ranging from simple to complex designs</p><p>Rigid and flexible circuit technologies <br>Custom circuit design igh-speed signal design <br> Differential impedance controlFine pitch BGA layoutSensitive <br> analog circuitsHigh current/voltage designsDesign for manufacturability</p>");
     });
     $("#button4").click(function() {
+      $("#text-title").html("<p> TECHMINDS will take your PCB through a set of comprehensive tests to overcome signal loss,noise susceptibility, power dissipation, overheating, impedance issues, and other malfunctions</p>");
+      $("#image-display").attr("src", "./assets/img/Images/03_Engineering Service/services-banner.jpg");
       $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the electrical connection between different electrical</p>");
     });
     $("#button5").click(function() {
+      $("#text-title").html("<p> TECHMINDS is a premier provider of PCB design services.<br>We are well prepared to undertake your printed circuit board design challenges ranging from simple to complex designs</p>");
+      $("#image-display").attr("src", "./assets/img/Images/03_Engineering Service/services-banner.jpg");
+      $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the electrical connection between different electrical</p>");
+    });
+    $("#button6").click(function() {
+      $("#text-title").html("<p>PCB Reverse Engineering is a reversely research technology on physical printedcircuit board (PCB) , which is deconstructed to get Gerber files and BOM. TECHMINDS is equipped to provide world-class PCB reverse engineering</p>");
+      $("#image-display").attr("src", "./assets/img/Images/03_Engineering Service/services-banner.jpg");
       $("#text-display").html("<h3> Schematic Design</h3><p>Schematic Design A schematic or a circuit diagram conveys the electrical connection between different electrical</p>");
     });
   });
+// $(document).ready(function() {
+//   $("#button1").click(function() {
+//     $("#image-display").attr("src", "./img/Images/01_Home/Home_Slide01.jpg)");
+//     $("#text-display").html("<h3>Title 1</h3><p>Content for Button 1 goes here.</p>");
+//   });
+//   $("#button2").click(function() {
+//     $("#image-display").attr("src", "image2.png");
+//     $("#text-display").html("<h3>Title 2</h3><p>Content for Button 2 goes here.</p>");
+//   });
+//   $("#button3").click(function() {
+//     $("#image-display").attr("src", "image3.png");
+//     $("#text-display").html("<h3>Title 3</h3><p>Content for Button 3 goes here.</p>");
+//   });
+// });
+
